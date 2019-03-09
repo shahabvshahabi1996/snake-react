02c2139e1ea74b356cd41a47f79716c7061c5a0f
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2019-03-09 11:42:41 +0330

    fixing game loop and movments and conflict with borders

--- src/App.js
+++ src/App.js
@@ -24,17 +24,59 @@ class App extends Component {
           x : 5,
           y : 5
         }
-      }
+      },
+      dir : {
+        x : 0,
+        y : 0
+      },
+      gameover : false,
+      score : 0
     }
   }
 
   componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyPress);
+    document.addEventListener('keydown', this.handleKey);
+    this.gameLoop();
+  }
+
+
+  gameRestart = () => {
+    this.setState({
+      apple : {
+        x : this.getRandNumber(),
+        y : this.getRandNumber()
+      },
+      snake : {
+        head : {
+          x : 5,
+          y : 5
+        }
+      },
+      dir : {
+        x : 0,
+        y : 0
+      },
+      gameover : false,
+      score : 0
+    }); 
   }
 
   getRandNumber() {
     return Math.floor(Math.random() * 12);
   }
+
+  gameLoop = () => {
+    this.setState({snake : {
+      head : {
+        x : this.state.snake.head.x + this.state.dir.x,
+        y : this.state.snake.head.y + this.state.dir.y
+      }
+    }})
+    this.checkConflict();
+    setTimeout(() => {
+      this.gameLoop()
+    },1000);
+  } 
 
   checkConflict = () => {
     let {snake , apple} = this.state;
@@ -42,92 +84,57 @@ class App extends Component {
       this.setState({apple :  {
         x : this.getRandNumber(),
         y : this.getRandNumber()
-      }});
+      },score : this.state.score + 1});
+    } else if(snake.head.x > 11 || snake.head.x < 0 || snake.head.y > 11 || snake.head.y < 0) {
+      this.setState({gameover : true , dir : {x : 0 , y : 0}});
+    } else {
+      return ;
     }
   }
 
-
   goUp = () => {
-    this.checkConflict();
-    this.setState({snake : {
-      head : {
-        x : this.state.snake.head.x,
-        y : this.state.snake.head.y - 1
-      }
+    this.setState({dir : {
+        x : 0,
+        y : -1
     }});
   }
 
   goDown = () => {
-    this.checkConflict();
-    this.setState({snake : {
-      head : {
-        x : this.state.snake.head.x,
-        y : this.state.snake.head.y + 1
-      }
+    this.setState({dir : {
+      x : 0,
+      y : 1
     }});
   }
 
   goRight = () => {
-    this.checkConflict();
-    this.setState({snake : {
-      head : {
-        x : this.state.snake.head.x + 1,
-        y : this.state.snake.head.y
-      }
+    this.setState({dir : {
+      x : 1,
+      y : 0
     }});
   }
 
   goLeft = () => {
-    this.checkConflict();
-    this.setState({snake : {
-      head : {
-        x : this.state.snake.head.x - 1 ,
-        y : this.state.snake.head.y
-      }
+    this.setState({dir : {
+      x : -1,
+      y : 0
     }});
   }
 
-  handleKeyPress = (e) => {
-
-    let {intervalFn} = this.state;
-
-    var intervalMovements;
-
-    if(intervalFn) {
-      clearInterval(intervalFn)
-    }
-    
+  handleKey = (e) => {
     switch(e.key) {
       case "ArrowUp": {
-        intervalMovements = setInterval(() => {
-          this.goUp();
-        },500)
-        this.setState({intervalFn : intervalMovements});
-        return;
+        return this.goUp();
       }
       case "ArrowDown" : {
-        intervalMovements = setInterval(() => {
-          this.goDown();
-        },500)
-        this.setState({intervalFn : intervalMovements});
-        return;
+        return this.goDown();
       }
       case "ArrowRight" : {
-        intervalMovements = setInterval(() => {
-          this.goRight();
-        },500)
-        this.setState({intervalFn : intervalMovements});
-        return;
+        return this.goRight();
       } 
       case "ArrowLeft" : {
-        intervalMovements = setInterval(() => {
-          this.goLeft();
-        },500)
-        this.setState({intervalFn : intervalMovements});
-        return;
+       return this.goLeft();
       }
       default :
-        clearInterval(intervalFn)
         return ;
     }
   }
@@ -142,18 +149,36 @@ class App extends Component {
     return snake.head.x === row && snake.head.y === col
   }
 
+  isSnakeEatsApple = (col,row) => {
+    let {apple , snake} = this.state;
+    return apple.x === row && row === snake.head.x && apple.y === col && col === snake.head.y;
+  }
+
   render() {
-    const {cells} = this.state;
+    const {cells , gameover , score} = this.state;
+    console.log(this.state);
     return (
       <div>
         <header>
           <h1>React Snake Game</h1>
         </header>
         <div className="grid">
+          <div className={`gameover ${gameover ? 'active-over' : ""}`}>
+            <div className="text">
+              <h1>Game Over , <span>Your Score is : {score}</span></h1>
+              <h3><button className="button" onClick={this.gameRestart}>Try Again</button></h3>
+            </div>
+          </div>
           {cells.map((col) => {
             return col.map((row,i) => {
               return (
-                <div key={i} className={`cell ${this.isApple(row.i,row.j) ? 'apple' : this.isHead(row.i,row.j) ? 'snake-head' : ''}`}></div>
+                <div key={i} className={
+                  `cell 
+                  ${this.isSnakeEatsApple(row.i,row.j) 
+                  ? 'snake-head' : this.isApple(row.i,row.j) 
+                  ? 'apple' : this.isHead(row.i,row.j) 
+                  ? 'snake-head' : ''}
+                  `}></div>
               )
             }) 
           })}
